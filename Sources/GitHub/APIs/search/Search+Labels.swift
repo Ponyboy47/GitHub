bff4abda42472313db68418dbaf98f921a8d2e78
Jacob Williams <ponyboy47@gmail.com>
2019-04-08 10:30:48 -0600

    Implemented the Label search api

---
+++ Sources/GitHub/APIs/search/Search+Labels.swift
@@ -0,0 +1,50 @@
+import HTTP
+
+public final class SearchLabels: GitHubAPI {
+    public typealias Category = SearchCategory
+    public typealias Options = URLQuery
+
+    public enum SortOptions: String {
+        case created
+        case updated
+        case bestMatch = "best-match"
+
+        public static let `default`: SortOptions = .bestMatch
+    }
+
+    public struct Response: GitHubResponse {
+        public init(response: HTTPResponse) {
+        }
+    }
+
+    public static let endpoint = "lables"
+    public static let requiresAuth = false
+    public static let method: HTTPMethod = .GET
+
+    public let connector: GitHubConnector
+
+    public init(connector: GitHubConnector) {
+        self.connector = connector
+    }
+
+    public func query(_ string: String,
+                      repositoryID id: Int,
+                      sort: SortOptions = .default,
+                      order: SortOrdering = .default,
+                      page: Int = 1,
+                      perPage: Int = githubPerPage) throws -> Future<Response> {
+        var options = Options()
+        options.add(option: "repository_id", value: "\(id)")
+        options.add(option: "q", value: string)
+        if order != .default {
+            options.add(option: "order", value: order)
+
+            // The sort parameter is ignored if the ordering is not specified
+            if sort != .default {
+                options.add(option: "sort", value: sort)
+            }
+        }
+
+        return try call(options: options, page: page, perPage: perPage)
+    }
+}
