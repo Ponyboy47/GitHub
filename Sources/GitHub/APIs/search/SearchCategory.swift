766f722dc02a529f634d5c8913beaaa2f92cf5f1
Jacob Williams <ponyboy47@gmail.com>
2019-04-09 08:34:01 -0600

    Added all the search apis to the search category

--- Sources/GitHub/APIs/search/SearchCategory.swift
+++ Sources/GitHub/APIs/search/SearchCategory.swift
@@ -3,9 +3,19 @@ public final class SearchCategory: GitHubAPICategory {
     
     public let repositories: SearchRepositories
     public let commits: SearchCommits
+    public let code: SearchCode
+    public let issues: SearchIssues
+    public let users: SearchUsers
+    public let topics: SearchTopics
+    public let labels: SearchLabels
 
     public init(connector: GitHubConnector) {
         repositories = .init(connector: connector)
         commits = .init(connector: connector)
+        code = .init(connector: connector)
+        issues = .init(connector: connector)
+        users = .init(connector: connector)
+        topics = .init(connector: connector)
+        labels = .init(connector: connector)
     }
 }
