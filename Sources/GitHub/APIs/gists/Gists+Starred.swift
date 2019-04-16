2e64e3fad1b2fb2aeb082816a1811bba88784b0e
Jacob Williams <ponyboy47@gmail.com>
2019-04-16 08:29:47 -0600

    Implements getting starred gists

---
+++ Sources/GitHub/APIs/gists/Gists+Starred.swift
@@ -0,0 +1,24 @@
+import struct Foundation.Date
+import HTTP
+
+public final class StarredGists: GitHubAPI {
+    public typealias Options = URLQuery
+    public typealias Response = [Gist]
+
+    public static let endpoint = "gists/starred"
+    public static let method: HTTPMethod = .GET
+
+    public let connector: GitHubConnector
+
+    public init(connector: GitHubConnector) {
+        self.connector = connector
+    }
+
+    public func query(since date: Date? = nil) throws -> Response {
+        var options = Options()
+        if let date = date {
+            options.add(option: "since", value: date.iso8601)
+        }
+        return try call(options: options)
+    }
+}
